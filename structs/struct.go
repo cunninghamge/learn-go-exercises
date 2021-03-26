592f375244fea0cc43219a987e8bcc65c6799a4f
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-03-25 21:17:06 -0600

    complete structs

---
+++ structs/struct.go
@@ -0,0 +1,41 @@
+package main
+
+import "math"
+
+type Shape interface {
+  Area() float64
+}
+
+type Rectangle struct {
+  Height float64
+  Width float64
+}
+
+func (r Rectangle) Area() float64 {
+  return r.Width * r.Height
+}
+
+type Circle struct {
+  Radius float64
+}
+
+func (c Circle) Area() float64 {
+  return c.Radius * c.Radius * math.Pi
+}
+
+type Triangle struct {
+  Height float64
+  Width float64
+}
+
+func (t Triangle) Area() float64 {
+  return (t.Width * t.Height) / 2
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+  return 2 * (rectangle.Width + rectangle.Height)
+}
+
+func Area(rectangle Rectangle) float64 {
+  return rectangle.Width * rectangle.Height
+}
